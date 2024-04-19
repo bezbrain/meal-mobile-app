9b340061da4b11da309a59da33a23f297d0ad745
bezbrain <belloquadriolawale@gmail.com>
2024-04-19 08:12:03 +0100

    Add the meals categories navigation

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import {
   SafeAreaView,
   StyleSheet,
-  Text,
   View,
   useWindowDimensions,
 } from "react-native";
@@ -10,6 +9,10 @@ import { CategoriesScreen } from "./screens";
 import { ScreenProvider } from "./contexts/screen.context";
 import { appFonts } from "./utils/fonts";
 import AppLoading from "expo-app-loading";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const { width, height } = useWindowDimensions();
@@ -26,20 +29,26 @@ export default function App() {
   }
 
   return (
-    <ScreenProvider>
-      <View style={[styles.container, appPaddingStyles]}>
-        <StatusBar style="light" />
-        <SafeAreaView>
-          <CategoriesScreen />
-        </SafeAreaView>
-      </View>
-    </ScreenProvider>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <ScreenProvider>
+          <View style={[styles.container, appPaddingStyles]}>
+            <Stack.Navigator>
+              <Stack.Screen
+                name="Meals Categories"
+                component={CategoriesScreen}
+              />
+            </Stack.Navigator>
+          </View>
+        </ScreenProvider>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // paddingVertical: 52,
   },
 });
